d5a203426323ca9f0b96f05f31c86f53b343011e
abdurrehmandurra <abdurrehmandurra@gmail.com>
2020-12-01 22:57:58 +0500

    adding select option instead of input boxes

--- gpa.js
+++ gpa.js
@@ -1,145 +1,217 @@
-const GPA = [4, 3.7, 3.3, 3, 2.7, 2.3, 2, 1.7, 1.3, 0];
+const table = document.getElementById("calTable");
+const GPA = document.getElementById("GPA");
 
 function calculate() {
-	let arrOM = [];
-	let arrGPA = [];
-	let arrCREDIT = [];
-	let arrGRADE = [];
-	let arr_CREDIT_GPA = [];
+	const credits = document.querySelectorAll("#credits");
+	// const grade = document.querySelectorAll("#grade");
+	const gpa = document.querySelectorAll("#gpa");
 
-	let sum_Credit_Gpa = 0;
-	let sumOM = 0;
-	let sumCREDIT = 0;
+	const arrCREDIT = [];
+	const arrGPA = [];
+	const arr_CREDIT_GPA = [];
+	let sumOfCreditGpa = 0;
+
 	let sumGPA = 0;
+	let sumCREDIT = 0;
 	let totalGPA = 0;
 
-	let inputOM = document.querySelectorAll("input#obtainedMarks");
-	let inputCreditBox = document.querySelectorAll("input#creditBox");
-
-	let percent = document.getElementById("percentage");
-	let previousGPA = document.getElementById("PGPA");
-
-	// Sum of All Credits
-	for (let i = 0; i < inputCreditBox.length; i++) {
-		arrCREDIT.push(parseInt(inputCreditBox[i].value));
+	// Sum of all subject CREDITS Hour and sum of all GPA
+	for (let i = 0; i < credits.length; i++) {
+		// console.log(credits);
+		arrCREDIT.push(parseInt(credits[i].value));
+		arrGPA.push(parseFloat(gpa[i].value));
 		sumCREDIT += arrCREDIT[i];
+		sumGPA += arrGPA[i];
 	}
 
-	// Sum of obtained marks
-	for (let i = 0; i < inputOM.length; i++) {
-		arrOM.push(parseInt(inputOM[i].value));
-		sumOM = sumOM + arrOM[i];
-
-		if (arrOM[i] >= 90 && arrOM[i] <= 100) {
-			arrGPA.push(GPA[0]);
-			arrGRADE.push("A+");
-		} else if (arrOM[i] <= 89 && arrOM[i] >= 85) {
-			arrGPA.push(GPA[1]);
-			arrGRADE.push("A-");
-		} else if (arrOM[i] <= 84 && arrOM[i] >= 80) {
-			arrGPA.push(GPA[2]);
-			arrGRADE.push("B+");
-		} else if (arrOM[i] <= 79 && arrOM[i] >= 75) {
-			arrGPA.push(GPA[3]);
-			arrGRADE.push("B");
-		} else if (arrOM[i] <= 74 && arrOM[i] >= 70) {
-			arrGPA.push(GPA[4]);
-			arrGRADE.push("B-");
-		} else if (arrOM[i] <= 69 && arrOM[i] >= 65) {
-			arrGPA.push(GPA[5]);
-			arrGRADE.push("C+");
-		} else if (arrOM[i] <= 64 && arrOM[i] >= 60) {
-			arrGPA.push(GPA[6]);
-			arrGRADE.push("C");
-		} else if (arrOM[i] <= 59 && arrOM[i] >= 55) {
-			arrGPA.push(GPA[7]);
-			arrGRADE.push("C-");
-		} else if (arrOM[i] <= 54 && arrOM[i] >= 50) {
-			arrGPA.push(GPA[8]);
-			arrGRADE.push("D");
-		} else if (arrOM[i] <= 49) {
-			arrGPA.push(GPA[9]);
-			arrGRADE.push("F");
-		}
-
-		// Sum of All GPA's
-		sumGPA = sumGPA + arrGPA[i];
-
-		document.querySelectorAll("#gpaBox")[i].innerText = arrGPA[i];
-		document.querySelectorAll("#gradeBox")[i].innerText = arrGRADE[i];
-	}
-
-	// Total Percentage
-	percent.innerText = ((sumOM / (arrOM.length * 100)) * 100).toFixed(2) + "%";
-
-	// CGPA
-	for (let i = 0; i < arrCREDIT.length; i++) {
+	// Multiplying credits hour to gpa
+	for (let i = 0; i < credits.length; i++) {
 		arr_CREDIT_GPA[i] = arrCREDIT[i] * arrGPA[i];
-		sum_Credit_Gpa += arr_CREDIT_GPA[i];
+		sumOfCreditGpa += arr_CREDIT_GPA[i];
 	}
-
-	// Total GPA
-	totalGPA = (sum_Credit_Gpa / sumCREDIT).toFixed(2);
-
-	// CGPA
-	if (previousGPA.value == "") {
-		gpa.innerText = totalGPA;
-	} else {
-		gpa.innerText = (
-			(parseFloat(totalGPA) + parseFloat(previousGPA.value)) /
-			2
-		).toFixed(2);
-	}
-
-	console.log("CGPA : " + totalGPA + previousGPA.value);
-	console.log("PREVIOUS GPA : " + previousGPA.value);
-	console.log("MULTIPLY GPA AND CREDITS : " + arr_CREDIT_GPA + ",");
-	console.log("OBTAINED MARKS : " + sumOM);
-	console.log("ARRAY OBTAINED MARKS : " + arrOM + " , ");
-	console.log("ARRAY GPA : " + arrGPA + " , ");
-	console.log("ARRAY CREDITS : " + arrCREDIT + " , ");
-	console.log("SUM CREDITS : " + sumCREDIT);
-	console.log("TOTAL GPA : " + totalGPA);
-	console.log("GRADES : " + arrGRADE + "\n\n\n\n");
+	totalGPA = (sumOfCreditGpa / sumCREDIT).toFixed(2);
+	// totalCGPA = ((totalGPA + previousGPA.value) / 2).toFixed(2);
+	GPA.innerText = totalGPA;
+	// console.log("Sum of all GPA : " + sumGPA);
+	// console.log("Sum of all CREDITS : " + sumCREDIT);
+	// console.log("Sum of all CREDITS and GPA : " + sumOfCreditGpa);
+	// console.log("total GPA : " + totalGPA);
+	// console.log("total CGPA : " + totalCGPA);
 }
 
 function addRow() {
-	// Inserting Obtained Marks Input In Td
-	var obtainedMarkInput = document.createElement("INPUT");
-	obtainedMarkInput.setAttribute("type", "text");
-	obtainedMarkInput.setAttribute("class", "form-control");
-	obtainedMarkInput.setAttribute("id", "obtainedMarks");
-
-	// Inserting Credit Per Subject
-	var creditInput = document.createElement("input");
-	creditInput.setAttribute("type", "text");
-	creditInput.setAttribute("class", "form-control");
-	creditInput.setAttribute("id", "creditBox");
-
 	const table = document.getElementById("calTable");
 	const row = table.insertRow(-1);
 
 	const subject = row.insertCell(0);
 	subject.innerText = "SUBJECT";
+	subject.setAttribute("class", "p-1");
 
-	const obtainedMarkcell = row.insertCell(1);
-	obtainedMarkcell.appendChild(obtainedMarkInput);
-	obtainedMarkcell.setAttribute("id", "om");
+	const box = creditSelectBox();
+	const creditCell = row.insertCell(1);
+	creditCell.appendChild(box);
 
-	const creditCell = row.insertCell(2);
-	creditCell.appendChild(creditInput);
-	creditCell.setAttribute("id", "cb");
+	const box2 = gpaSelectBox();
+	const gpaCell = row.insertCell(2);
+	gpaCell.appendChild(box2);
 
-	const gpaCell = row.insertCell(3);
-	gpaCell.setAttribute("id", "gpaBox");
-
-	const gradecell = row.insertCell(4);
-	gradecell.setAttribute("id", "gradeBox");
+	// const box3 = gradeSelectBox();
+	// const gradeCell = row.insertCell(3);
+	// gradeCell.appendChild(box3);
 }
 
-const table = document.getElementById("calTable");
 function delRow() {
 	if (table.rows.length > 2) {
 		table.deleteRow(-1);
 	}
 }
+
+function creditSelectBox() {
+	let creditComboBox = document.createElement("select");
+	creditComboBox.setAttribute("id", "credits");
+	creditComboBox.setAttribute("class", "form-control");
+
+	let option2 = document.createElement("option");
+	option2.text = "2";
+	option2.value = "2";
+
+	let option3 = document.createElement("option");
+	option3.text = "3";
+	option3.value = "3";
+
+	let option4 = document.createElement("option");
+	option4.text = "4";
+	option4.value = "4";
+
+	creditComboBox.append(option2);
+	creditComboBox.append(option3);
+	creditComboBox.append(option4);
+
+	return creditComboBox;
+}
+
+function gpaSelectBox() {
+	let gpaComboBox = document.createElement("select");
+	gpaComboBox.setAttribute("id", "gpa");
+	gpaComboBox.setAttribute("class", "form-control");
+
+	let option1 = document.createElement("option");
+	option1.text = "0";
+	option1.value = "0";
+
+	let option2 = document.createElement("option");
+	option2.text = "1.3";
+	option2.value = "1.3";
+
+	let option3 = document.createElement("option");
+	option3.text = "1.7";
+	option3.value = "1.7";
+
+	let option4 = document.createElement("option");
+	option4.text = "2";
+	option4.value = "2";
+
+	let option5 = document.createElement("option");
+	option5.text = "2.3";
+	option5.value = "2.3";
+
+	let option6 = document.createElement("option");
+	option6.text = "2.7";
+	option6.value = "2.7";
+
+	let option7 = document.createElement("option");
+	option7.text = "3";
+	option7.value = "3";
+
+	let option8 = document.createElement("option");
+	option8.text = "3.3";
+	option8.value = "3.3";
+
+	let option9 = document.createElement("option");
+	option9.text = "3.7";
+	option9.value = "3.7";
+
+	let option10 = document.createElement("option");
+	option10.text = "4";
+	option10.value = "4";
+
+	let arr = [
+		option1,
+		option2,
+		option3,
+		option4,
+		option5,
+		option6,
+		option7,
+		option8,
+		option9,
+		option10,
+	];
+	for (let i = 0; i < arr.length; i++) {
+		gpaComboBox.append(arr[i]);
+	}
+	return gpaComboBox;
+}
+
+// function gradeSelectBox() {
+// 	let gradeComboBox = document.createElement("select");
+// 	gradeComboBox.setAttribute("id", "grade");
+// 	gradeComboBox.setAttribute("class", "form-control");
+
+// 	let option1 = document.createElement("option");
+// 	option1.text = "A+";
+// 	option1.value = "A+";
+
+// 	let option2 = document.createElement("option");
+// 	option2.text = "A-";
+// 	option2.value = "A-";
+
+// 	let option3 = document.createElement("option");
+// 	option3.text = "B+";
+// 	option3.value = "B+";
+
+// 	let option4 = document.createElement("option");
+// 	option4.text = "B";
+// 	option4.value = "B";
+
+// 	let option5 = document.createElement("option");
+// 	option5.text = "B-";
+// 	option5.value = "B-";
+
+// 	let option6 = document.createElement("option");
+// 	option6.text = "C+";
+// 	option6.value = "C+";
+
+// 	let option7 = document.createElement("option");
+// 	option7.text = "C";
+// 	option7.value = "C";
+
+// 	let option8 = document.createElement("option");
+// 	option8.text = "C-";
+// 	option8.value = "C-";
+
+// 	let option9 = document.createElement("option");
+// 	option9.text = "D";
+// 	option9.value = "D";
+
+// 	let option10 = document.createElement("option");
+// 	option10.text = "F";
+// 	option10.value = "D";
+
+// 	let arr = [
+// 		option1,
+// 		option2,
+// 		option3,
+// 		option4,
+// 		option5,
+// 		option6,
+// 		option7,
+// 		option8,
+// 		option9,
+// 		option10,
+// 	];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		gradeComboBox.append(arr[i]);
+// 	}
+// 	return gradeComboBox;
+// }
